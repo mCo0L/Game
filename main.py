bc38d2cb6baf3dbd194b92a7d5367e9df02ff3cd
vijjisingh <vishu1132@gmail.com>
2017-11-11 13:25:56 +0530

    changed

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-import pygame, sys
+import pygame,sys
 from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode(resolution=(400, 300))
-pygame.display.set_caption('Hello Game')
+BLACK=(0,0,0)
+DISPLAYSURF=pygame.display.set_mode((600,600),0,32)
+DISPLAYSURF.fill((255,255,255))
+pygame.display.set_caption("new game")
+pixObj = pygame.PixelArray(DISPLAYSURF)
+
 
 while True:
-    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
