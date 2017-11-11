05eef76cc56c598e3352b1d9b8792aa5c4ce3de5
mCo0l <mukul.chauhan@hotmail.com>
2017-11-11 14:41:07 +0530

    Door Image added

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from pygame.locals import *
 
 pygame.init()
 BLACK=(0,0,0)
-DISPLAYSURF=pygame.display.set_mode((600,600),0,32)
-DISPLAYSURF.fill((255,255,255))
+windowWidth, windowHeight = 854, 480 #854, 480 #640, 360 #750, 500 #1280, 720
+window = pygame.display.set_mode((windowWidth,windowHeight)) # put ",pygame.FULLSCREEN" after "windowHeight)" for fulscreen
+pygame.display.set_caption('First Game')
 pygame.display.set_caption("new game")
-pixObj = pygame.PixelArray(DISPLAYSURF)
+pixObj = pygame.PixelArray(window)
 
 
 while True:
