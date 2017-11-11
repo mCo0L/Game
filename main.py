bfd75a85b0ae8a3c4cadfcec1900db91f866b8b4
mCo0l <mukul.chauhan@hotmail.com>
2017-11-11 13:09:26 +0530

    Innitial Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode(resolution=(400, 300))
+pygame.display.set_caption('Hello Game')
+
+while True:
+    
