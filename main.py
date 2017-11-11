c0124803c9b8a97d189d87dcea52273001796bee
vijjisingh <vishu1132@gmail.com>
2017-11-11 14:51:15 +0530

    new one

--- main.py
+++ main.py
@@ -3,22 +3,14 @@ from pygame.locals import *
 
 pygame.init()
 BLACK=(0,0,0)
-<<<<<<< HEAD
-windowWidth, windowHeight = 854, 480 #854, 480 #640, 360 #750, 500 #1280, 720
-window = pygame.display.set_mode((windowWidth,windowHeight)) # put ",pygame.FULLSCREEN" after "windowHeight)" for fulscreen
-pygame.display.set_caption('First Game')
-pygame.display.set_caption("new game")
-pixObj = pygame.PixelArray(window)
-=======
 FPS=30
 fpsClock= pygame.time.Clock()
 DISPLAYSURF=pygame.display.set_mode((600,600),0,32)
 
 pygame.display.set_caption("new game")
->>>>>>> 0e7b016e2892bbf87f5b69e56e737978ba38234f
 
 WHITE=(255,255,255)
-gimg=pygame.image.load('pinkgirl.png')
+gimg=pygame.image.load('devil.png')
 gx=10
 gy=10
 direction='right'
