c15324931326954edc81fd1ad9dbb44a85cce732
Mukul Chauhan <mukul.chauhan@hotmail.com>
2017-11-12 12:38:04 +0530

    ReDone almost everything

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ def gameover(level, score):
 
 def loadbricks(level):
     if level==1:
-        brickloc=[(14,11),(15,11),(4,10),(5,10),(6,10),(11,9),(12,9),(13,9),(3,8),(4,8),(7,7),(8,7),(13,6),(14,6),(15,6),(3,5),(4,5),(12,4),(13,4),(2,3),(3,3),(4,3),(12,2),(13,2),(0,11),(1,11),(9,11),(10,11),(11,11),(19,11),(19,9),(18,9),(17,9),(16,9),(19,7),(18,7),(13,8),(0,7),(1,7),(8,5),(5,11),(5,12),(5,13),(19,4),(18,4),(17,4),(15,4),(15,5),(16,4),(8,0),(8,1),(8,2),(8,3),(8,4),(8,5),(8,6)]
-
+        brickloc=[[14,11],[15,11],[4,10],[5,10],[6,10],[11,9],[12,9],[13,9],[3,8],[4,8],[7,7],[8,7],[13,6],[14,6],[15,6],[3,5],[4,5],[12,4],[13,4],[2,3],[3,3],[4,3],[12,2],[13,2],[0,11],[1,11],[9,11],[10,11],[11,11],[19,11],[19,9],[18,9],[17,9],[16,9],[19,7],[18,7],[13,8],[0,7],[1,7],[8,5],[5,11],[5,12],[5,13],[19,4],[18,4],[17,4],[15,4],[15,5],[16,4],[8,0],[8,1],[8,2],[8,3],[8,4],[8,5],[8,6]]
     return brickloc
     
 
@@ -18,61 +17,60 @@ def loadbricks(level):
     locatebricks(level)
     locatekeys(level)
     locatedevils(level)'''
+
+def getLocation(currLoc, action):
+    newLoc = currLoc[:]
+    if action == K_LEFT:
+        newLoc[0] -= 1
+    if action == K_RIGHT:
+        newLoc[0] += 1
+    if action == K_UP:
+        newLoc[1] -= 1
+    if action == K_DOWN:
+        newLoc[1] += 1
+        
+    if newLoc in brickloc:
+        print("Nope lol")
+        return currLoc
     
+    elif (newLoc[0] <= 19 and newLoc[0] >= 0 ) and (newLoc[1] <= 13 and newLoc[1] >= 0):
+        return newLoc
+    else:
+        return currLoc
+
+
 pygame.init()
-BLACK=(0,0,0)
+
 FPS=30
-fpsClock= pygame.time.Clock()
-DISPLAYSURF=pygame.display.set_mode((800,600))
-
-pygame.display.set_caption("new game")
-dimg1=pygame.image.load("doorclosed.png")
+BLACK=(0,0,0)
 WHITE=(255,255,255)
-gimg=pygame.image.load('pinkgirl.png')
-dimg1 = pygame.transform.scale(dimg1,(60, 100))
-brick=pygame.image.load('brick.png')
-brick = pygame.transform.scale(brick,(40, 40))
-gx=0
-gy=465
-dx=740
-dy=0
-direction='right'
-bx=0
+fpsClock= pygame.time.Clock()
 
-loc = [0,12]
-def checkbound(x,y):
-    if (x<0):
-        return 0
-    if (y<0):
-        return 0
+DISPLAYSURF=pygame.display.set_mode((800,600))
+pygame.display.set_caption("GoMaria")
+
+maria = pygame.image.load('pinkgirl.png')
+maria = pygame.transform.scale(maria,(40, 60))
+doorClosed = pygame.image.load("doorclosed.png")
+doorClosed = pygame.transform.scale(doorClosed,(60, 100))
+brick = pygame.image.load('brick.png')
+brick = pygame.transform.scale(brick,(40, 40))
+
+girlLoc =[0, 13]
+doorLoc = [18,0]
+level = 1
+
+
 while True:
     DISPLAYSURF.fill(WHITE)
-    
-    """if direction =='right':
-        gx +=5
-        if gx==280:
-            direction="down"
-    elif direction =='down':
-        gy +=5
-        if gy ==220:
-            direction ='left'
-    elif direction == 'left':
-        gx -=5
-        if gx ==10:
-            direction ='up'
-    elif direction =='up':
-        gy -=5
-        if gy ==10:
-            direction='right'"""
-
-    DISPLAYSURF.blit(gimg,(gx,gy))
-    DISPLAYSURF.blit(dimg1,(dx,dy))
+    DISPLAYSURF.blit(maria,(girlLoc[0]*40,girlLoc[1]*40))
+    DISPLAYSURF.blit(doorClosed,(doorLoc[0]*40,doorLoc[1]*40))
     brickloc = loadbricks(1)
     for bloc in brickloc:
             DISPLAYSURF.blit(brick,(bloc[0]*40, bloc[1]*40)) 
     base = 0
-    while base<1200:
-        DISPLAYSURF.blit(brick,(base,540))
+    while base<800:
+        DISPLAYSURF.blit(brick,(base,560))
         base = base+40
         
     
@@ -82,27 +80,11 @@ while True:
             sys.exit()
         elif event.type == KEYDOWN:
             keyPressed = True
-            if event.key == K_LEFT:
-                if (loc[0]*40-40)>=0:
-                    gx -=40
-                    loc[0] -= 1
-                
-                    print("location: ",loc)
-            elif event.key == K_RIGHT:
-                if (loc[0]*40+40)<=760:
-                    gx +=40
-                    loc[0] += 1
-                    print("location: ",loc)
-            elif event.key == K_UP:
-                if (loc[1]*40-40)>=0:
-                    gy -=40
-                    loc[1] -= 1
-                    print("location: ",loc)
-            elif event.key == K_DOWN:
-                if (loc[1]*40+40)<=480:
-                    gy +=40
-                    loc[1] += 1
-                    print("location: ",loc)
+            action = event.key
+            girlLoc = getLocation(girlLoc, action)
+            print("Location: ",girlLoc)
+
+
     pygame.display.update()
     fpsClock.tick(FPS) 
     
