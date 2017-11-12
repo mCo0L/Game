acd912757a5dfe2885c5a680b808fde38aef072e
vijjisingh <vishu1132@gmail.com>
2017-11-12 14:54:52 +0530

    new

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from pygame.locals import *
 def gameover(level, score):
     pass'''
 
-def loadbricks(level):
+def loaditems(level):
     if level==1:
         brickloc=[[14,11],[15,11],[4,10],[5,10],[6,10],[11,9],[12,9],[13,9],[3,8],[4,8],[7,7],[8,7],[13,6],[14,6],[15,6],[3,5],[4,5],[12,4],[13,4],[2,3],[3,3],[4,3],[12,2],[13,2],[0,11],[1,11],[9,11],[10,11],[11,11],[19,11],[19,9],[18,9],[17,9],[16,9],[19,7],[18,7],[13,8],[0,7],[1,7],[8,5],[5,11],[5,12],[5,13],[19,4],[18,4],[17,4],[15,4],[15,5],[16,4],[8,0],[8,1],[8,2],[8,3],[8,4],[8,5],[8,6]]
-    return brickloc
+        keysloc=[[19,8],[7,2]]
+    return brickloc,keysloc
     
 
 '''def loadLevel(level):
@@ -55,6 +56,8 @@ doorClosed = pygame.image.load("doorclosed.png")
 doorClosed = pygame.transform.scale(doorClosed,(60, 100))
 brick = pygame.image.load('brick.png')
 brick = pygame.transform.scale(brick,(40, 40))
+key = pygame.image.load('key.png')
+key = pygame.transform.scale(key,(40, 40))
 
 girlLoc =[0, 13]
 doorLoc = [18,0]
@@ -65,9 +68,13 @@ while True:
     DISPLAYSURF.fill(WHITE)
     DISPLAYSURF.blit(maria,(girlLoc[0]*40,girlLoc[1]*40))
     DISPLAYSURF.blit(doorClosed,(doorLoc[0]*40,doorLoc[1]*40))
-    brickloc = loadbricks(1)
+    brickloc,keysloc = loaditems(level)
+    
     for bloc in brickloc:
             DISPLAYSURF.blit(brick,(bloc[0]*40, bloc[1]*40)) 
+            
+    for keys in keysloc:
+            DISPLAYSURF.blit(key,(keys[0]*40, keys[1]*40)) 
     base = 0
     while base<800:
         DISPLAYSURF.blit(brick,(base,560))
